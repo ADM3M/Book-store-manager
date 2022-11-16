66285668519a7e8ac755c91027c416f41aee06cc
ADM3M <fly.swift499@gmail.com>
2022-11-16 18:19:56 +0300

    temp

---
+++ Services/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Jul.Data;
+
+namespace Jul.Services;
+
+public class UnitOfWork
+{
+    private static UnitOfWork? _instance = null;
+
+    public DataContext DbContext { get; set; }
+    
+    private UnitOfWork()
+    {
+        DbContext = new DataContext();
+    }
+    
+    public static UnitOfWork Create()
+    {
+        return _instance ??= new UnitOfWork();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await DbContext.SaveChangesAsync();
+    }
+}
